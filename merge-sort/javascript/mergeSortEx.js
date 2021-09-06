a3e97e91aa55bafa6c363fff5411fa4e5b9c1ef6
Max Manayan <maximillian521@gmail.com>
2021-09-06 12:25:33 -0600

    merge sort directory setup

---
+++ merge-sort/javascript/mergeSortEx.js
@@ -0,0 +1,12 @@
+// Merge Sort Example
+
+// Links:
+// simple breakdown - [ https://stackabuse.com/merge-sort-in-javascript/ ]
+
+// Steps:
+// 1. Split the given list into two halves
+// 2. Continue dividing the subarrays in the same manner until
+//    you are let with only single element arrays
+// 3. Starting with the single element arrays, merge the subarrays
+//    so that each merged subarray is sorted
+// 4. Repeat Step 3 until end up with single sorted array
