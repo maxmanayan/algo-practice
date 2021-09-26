9576f806acd7e65e9a14335d1e9f1529ba053fc0
Max Manayan <maximillian521@gmail.com>
2021-09-26 17:05:20 -0600

    stack with hashmap functional

--- stacks/javascript/validParentheses.js
+++ stacks/javascript/validParentheses.js
@@ -1,47 +1,75 @@
 // Given a string of parentheses and brackets, judge if the string is
 // valid (every parenthesis has an open/close in correct order)
 
-let sample = "[()()[()]{()}](";
+let sample = "[()()[()]{()}(]";
 
-const validParentheses = (sample) => {
-  if (sample.length < 2) return false;
-  if (sample.length % 2 !== 0) return false;
+// const validParentheses = (sample) => {
+//   if (sample.length < 2) return false;
+//   if (sample.length % 2 !== 0) return false;
+//   const sampleArr = sample.split("");
+//   const arrStack = [];
+//   for (let i = 0; i < sampleArr.length; i++) {
+//     const p = sampleArr[i];
+//     if (p === "(" || p === "[" || p === "{") {
+//       arrStack.push(p);
+//     } else {
+//       let lastInStack = arrStack.length - 1;
+//       if (arrStack[lastInStack] === "(") {
+//         if (p === ")") {
+//           arrStack.pop();
+//         } else {
+//           return false;
+//         }
+//       } else if (arrStack[lastInStack] === "[") {
+//         if (p === "]") {
+//           arrStack.pop();
+//         } else {
+//           return false;
+//         }
+//       } else if (arrStack[lastInStack] === "{") {
+//         if (p === "}") {
+//           arrStack.pop();
+//         } else {
+//           return false;
+//         }
+//       } else {
+//         return false;
+//       }
+//     }
+//   }
+//   if (arrStack.length === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// console.log(validParentheses(sample));
+
+const validParenthesesHashmap = (sample) => {
+  // Early returns
+  if (sample.length < 2 || sample.length % 2 !== 0) return false;
+  // Create hashmap
+  const map = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+  // split sample string into an array
   const sampleArr = sample.split("");
-  const arrStack = [];
-  for (let i = 0; i < sampleArr.length; i++) {
-    const p = sampleArr[i];
+  const stack = [];
+  // Validation logic
+  for (const p of sampleArr) {
     if (p === "(" || p === "[" || p === "{") {
-      arrStack.push(p);
+      stack.push(p);
+    } else if (stack[stack.length - 1] === map[p]) {
+      stack.pop();
     } else {
-      let lastInStack = arrStack.length - 1;
-      if (arrStack[lastInStack] === "(") {
-        if (p === ")") {
-          arrStack.pop();
-        } else {
-          return false;
-        }
-      } else if (arrStack[lastInStack] === "[") {
-        if (p === "]") {
-          arrStack.pop();
-        } else {
-          return false;
-        }
-      } else if (arrStack[lastInStack] === "{") {
-        if (p === "}") {
-          arrStack.pop();
-        } else {
-          return false;
-        }
-      } else {
-        return false;
-      }
+      return false;
     }
   }
-  if (arrStack.length === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  // Measure true or false
+  return stack.length ? false : true;
 };
 
-console.log(validParentheses(sample));
+console.log(validParenthesesHashmap(sample));
