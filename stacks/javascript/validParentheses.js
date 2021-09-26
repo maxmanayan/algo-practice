38ff99c33f9bf379d046d92f02e0a44f0b3cacb2
Max Manayan <maximillian521@gmail.com>
2021-09-26 16:22:19 -0600

    stacks directory created

---
+++ stacks/javascript/validParentheses.js
@@ -0,0 +1,10 @@
+// Given a string of parentheses and brackets, judge if the string is
+// valid (every parenthesis has an open/close in correct order)
+
+let sample = "()()[()]{}";
+
+const validParentheses = (sample) => {
+  return sample;
+};
+
+console.log(validParentheses(sample));
